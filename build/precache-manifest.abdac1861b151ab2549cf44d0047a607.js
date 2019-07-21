e3d6ee3ba51530a8117f403efc5ad87652c9f955
Mark <hisshash@gmail.com>
2019-07-21 19:11:39 -0400

    Init commit

---
+++ build/precache-manifest.abdac1861b151ab2549cf44d0047a607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd31c80ff6437dda92a6bd3101aad4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d4333b45f0672f29f6",
+    "url": "/static/css/main.b66050b4.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "31d4333b45f0672f29f6",
+    "url": "/static/js/main.01142922.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>FOXSTRIKE</h1>
+        <p>We're building things</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
